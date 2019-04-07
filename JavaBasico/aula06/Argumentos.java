329200ef18bb1a3470ce45643031e3d6bb2ff190
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2019-04-07 11:10:59 -0300

    Passagem de argumentos no Java

---
+++ JavaBasico/aula06/Argumentos.java
@@ -0,0 +1,8 @@
+class Argumentos{
+	
+	public static void main (String[] args){
+
+		System.out.println("Voce digitou " + args[0]);
+	}
+
+}
---
+++ JavaBasico/aula06/Argumentos2.java
@@ -0,0 +1,8 @@
+class Argumentos2{
+	
+	public static void main (String[] args){
+
+		System.out.println("Ola mundo " + args[0]);
+	}
+
+}
