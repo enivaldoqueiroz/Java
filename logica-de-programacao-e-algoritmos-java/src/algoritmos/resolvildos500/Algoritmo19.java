db4b209d0882fd09cca157eeafed7afa8967445c
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-06-19 07:58:29 -0300

    Implementação do Algoritmo18 e 19 do Livro

---
+++ logica-de-programacao-e-algoritmos-java/src/algoritmos/resolvildos500/Algoritmo19.java
@@ -0,0 +1,13 @@
+package algoritmos.resolvildos500;
+
+public class Algoritmo19 {
+
+	public static void main(String[] args) {
+		// Algoritmo19
+		
+		int x = 64;		
+		System.out.println("Raiz de 64: "+Math.sqrt(x));
+
+	}
+
+}
---
+++ logica-de-programacao-e-algoritmos-java/src/algoritmos/resolvildos500/Algortimo18.java
@@ -0,0 +1,20 @@
+package algoritmos.resolvildos500;
+
+public class Algortimo18 {
+
+	public static void main(String[] args) {
+		
+		/* Livro Introdução a programação 
+		 * 500 Algoritmos Resolvidos
+		 * 
+		 * Algoritmo 18
+		 *
+		 */
+		int x;
+		x = 10;
+		System.out.println("Valor de x ="+ x+1);
+		
+
+	}
+
+}
